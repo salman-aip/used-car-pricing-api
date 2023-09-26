31410f43ffb1c59c59baf66fbddb885e4c8795ea
salman-aip <avenirsalman@gmail.com>
2023-09-26 23:44:55 +0600

    add relation between user-service and user-repository based on user-entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = this.repository.create(createUserDto); // create instance of entity
+    return this.repository.save(user); // save to db
   }
 
   findAll() {
