cce0c66776ca18587bb11198c524882bb57ff798
salman-aip <avenirsalman@gmail.com>
2023-10-06 23:29:25 +0600

    serialization in interceptors

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,5 +1,7 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { Observable, map } from 'rxjs';
+import { CustomUserDTO } from 'src/users/dto/user-custom.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
@@ -13,6 +15,11 @@ export class SerializeInterceptor implements NestInterceptor {
       map((data: any) => {
         // run before the response is sent out
         console.log('Running before response sent out');
+
+        // turn data into instance of custom-user-dto
+        return plainToClass(CustomUserDTO, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
