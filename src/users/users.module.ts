4d70719c043d2e368e84268350234e56d2f62cfe
salman-aip <avenirsalman@gmail.com>
2023-10-07 22:18:52 +0600

    initialize auth-service class for authentication

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthService } from 'src/authentication/auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // create repository based on entity
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
